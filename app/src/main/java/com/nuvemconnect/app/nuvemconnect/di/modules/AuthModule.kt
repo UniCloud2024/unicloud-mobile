7c641b00458ce514d32026dbc63c7a20a58f9709
rafaseron <rafaelseron3011@gmail.com>
2024-10-02 15:27:33 -0400

    feat: adicionar Provide de Service e nomear Retrofit

--- app/src/main/java/com/nuvemconnect/app/nuvemconnect/di/modules/AuthModule.kt
+++ app/src/main/java/com/nuvemconnect/app/nuvemconnect/di/modules/AuthModule.kt
@@ -3,19 +3,26 @@ package com.nuvemconnect.app.nuvemconnect.di.modules
 import com.nuvemconnect.app.nuvemconnect.data.network.AuthService
 import com.nuvemconnect.app.nuvemconnect.data.repository.ServiceRepository
 import okhttp3.OkHttpClient
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val authModule =
     module {
-        single { provideRetrofit(get()) }
+        single(named("authRetrofit")) { provideRetrofit(get()) }
+        single { provideOkHttpClient() }
         single { provideRepository(get()) }
+        single { provideService(get(named("authRetrofit"))) }
     }
 
 fun provideRepository(authService: AuthService): ServiceRepository  {
     val repository = ServiceRepository(authService = authService)
     return repository
+}
+
+fun provideService(retrofit: Retrofit): AuthService{
+    return retrofit.create(AuthService::class.java)
 }
 
 fun provideRetrofit(client: OkHttpClient): Retrofit =
