4d499ad7b13fcc71639f181b8f93f19ba85401cf
rafaseron <rafaelseron3011@gmail.com>
2024-10-01 22:11:16 -0400

    feat: provideRepository no AuthModule

--- app/src/main/java/com/nuvemconnect/app/nuvemconnect/di/modules/AuthModule.kt
+++ app/src/main/java/com/nuvemconnect/app/nuvemconnect/di/modules/AuthModule.kt
@@ -1,18 +1,27 @@
 package com.nuvemconnect.app.nuvemconnect.di.modules
 
+import com.nuvemconnect.app.nuvemconnect.data.network.AuthService
+import com.nuvemconnect.app.nuvemconnect.data.repository.ServiceRepository
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val authModule = module {
-    single { provideRetrofit(get()) }
+val authModule =
+    module {
+        single { provideRetrofit(get()) }
+        single { provideRepository(get()) }
+    }
+
+fun provideRepository(authService: AuthService): ServiceRepository  {
+    val repository = ServiceRepository(authService = authService)
+    return repository
 }
 
-fun provideRetrofit(client: OkHttpClient): Retrofit {
-    return Retrofit.Builder()
+fun provideRetrofit(client: OkHttpClient): Retrofit =
+    Retrofit
+        .Builder()
         .baseUrl("https://nuvemconnectapi.seronsoftware.com/")
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .build()
-}
