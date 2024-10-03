9b568ff7b11a3ff4df4c6c455557f0671f968d09
rafaseron <rafaelseron3011@gmail.com>
2024-10-03 01:52:09 -0400

    feat: Requests para redefinir senha

--- app/src/main/java/com/nuvemconnect/app/nuvemconnect/model/service/ServiceRequest.kt
+++ app/src/main/java/com/nuvemconnect/app/nuvemconnect/model/service/ServiceRequest.kt
@@ -3,3 +3,7 @@ package com.nuvemconnect.app.nuvemconnect.model.service
 data class AccountRequest(val name: String, val email: String, val password: String, val passwordConfirmation: String)
 
 data class LoginRequest(val email: String, val password: String)
+
+data class ResetPasswordRequest(val email: String)
+
+data class SendResetPasswordRequest(val tokenUUID: String, val token: String, val email: String, val password: String, val passwordConfirmation: String)
