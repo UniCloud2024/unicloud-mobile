ba6d9a87478ecfa0a5f934ce026f3fc1671e9647
rafaseron <rafaelseron3011@gmail.com>
2024-10-03 01:59:43 -0400

    feat: AuthService implementado

--- app/src/main/java/com/nuvemconnect/app/nuvemconnect/data/network/AuthService.kt
+++ app/src/main/java/com/nuvemconnect/app/nuvemconnect/data/network/AuthService.kt
@@ -4,8 +4,12 @@ import com.nuvemconnect.app.nuvemconnect.model.service.AccountRequest
 import com.nuvemconnect.app.nuvemconnect.model.service.AccountResponse
 import com.nuvemconnect.app.nuvemconnect.model.service.LoginRequest
 import com.nuvemconnect.app.nuvemconnect.model.service.LoginResponse
+import com.nuvemconnect.app.nuvemconnect.model.service.ResetPasswordRequest
+import com.nuvemconnect.app.nuvemconnect.model.service.SendResetPasswordRequest
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface AuthService {
 
@@ -14,5 +18,11 @@ interface AuthService {
 
     @POST("account/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @POST("account/request-password-reset")
+    suspend fun resetPasswordRequest(@Body request: ResetPasswordRequest): Response<Void>
+
+    @PUT("account/reset-password")
+    suspend fun sendResetPassword(@Body request: SendResetPasswordRequest): Response<Void>
 
 }
